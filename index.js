21714fe84e0c7c386a681858ed176a7cbdb9f2d7
Yuri <+>
2024-01-20 20:06:57 -0500

    Completed Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+  function writeCards(names, event) {
+    const cards = []
+    for(let i = 0; i < names.length; i++){
+      cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+  return cards;
+}
+
+function countDown(number) {
+  let down = 0;
+  while(down < 11){
+    console.log(down++);
+  }
+  }
